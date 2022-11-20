cac3550d3d191f5c9a5d258ce6e45bd61ea647ae
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-20 21:12:34 +0530

    Completed Day 03, Part 02

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
 
@@ -7,6 +8,11 @@ pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
     let houses_visited = houses.keys().count();
 
     println!("Santa visited {} houses",houses_visited);
+
+    let houses = parse_instructions_p2(&contents);
+    let houses_visited = houses.len();
+
+    println!("Houses visited: {}", houses_visited);
     Ok(())
 }
 
@@ -30,6 +36,39 @@ fn parse_instructions_p1(contents: &str) -> HashMap<(i32,i32),u32> {
     houses
 }
 
+fn parse_instructions_p2(contents: &str) -> HashSet<(i32,i32)> {
+    let mut houses : HashSet<(i32,i32)> = HashSet::new();
+    let mut santa_pos = (0,0);
+    let mut robo_pos = (0,0);
+
+    houses.insert(santa_pos);
+    houses.insert(robo_pos);
+
+    let mut current_pos = (0,0);
+
+    for (index,c) in contents.char_indices() {
+        if index % 2 == 0 {
+            santa_pos = get_new_pos(santa_pos,c);
+            houses.insert(santa_pos);
+        } else {
+            robo_pos = get_new_pos(robo_pos,c);
+            houses.insert(robo_pos);
+        }
+    }
+    houses
+}
+
+fn get_new_pos(current_pos: (i32,i32),c:char) -> (i32,i32) {
+    let new_pos = match c {
+        '^' => (current_pos.0,current_pos.1+1),
+        'v' => (current_pos.0, current_pos.1-1),
+        '>' => (current_pos.0+1, current_pos.1),
+        _ => (current_pos.0-1,current_pos.1),
+    };
+    new_pos
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -41,5 +80,10 @@ mod tests {
         assert_eq!(2,parse_instructions_p1("^v^v^v^v^v").keys().count());
     }
 
+    #[test]
+    fn test_part_two() {
+        assert_eq!(3, parse_instructions_p2("^v").len());
+        assert_eq!(11, parse_instructions_p2("^v^v^v^v^v").len());
+    }
 }
 
