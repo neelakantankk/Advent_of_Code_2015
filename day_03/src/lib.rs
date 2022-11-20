2df084aaeec94d633aed7bad4c0f810630ae9d21
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-20 21:22:17 +0530

    Modified by cargo fmt

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,12 +1,11 @@
-use std::error::Error;
 use std::collections::HashSet;
+use std::error::Error;
 
-pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
-
+pub fn run(contents: String) -> Result<(), Box<dyn Error>> {
     let houses = parse_instructions_p1(&contents);
     let houses_visited = houses.len();
 
-    println!("Santa visited {} houses",houses_visited);
+    println!("Santa visited {} houses", houses_visited);
 
     let houses = parse_instructions_p2(&contents);
     let houses_visited = houses.len();
@@ -15,49 +14,48 @@ pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_instructions_p1(contents: &str) -> HashSet<(i32,i32)> {
-    let mut houses: HashSet<(i32,i32)> = HashSet::new();
+fn parse_instructions_p1(contents: &str) -> HashSet<(i32, i32)> {
+    let mut houses: HashSet<(i32, i32)> = HashSet::new();
     // Tracking houses on grid using (x,y) coordinates
     // x is East--West, y is N--S
-    let mut current_pos = (0,0);
+    let mut current_pos = (0, 0);
     houses.insert(current_pos);
 
     for c in contents.chars() {
-        current_pos = get_new_pos(current_pos,c);
+        current_pos = get_new_pos(current_pos, c);
         houses.insert(current_pos);
     }
     houses
 }
 
-fn parse_instructions_p2(contents: &str) -> HashSet<(i32,i32)> {
-    let mut houses : HashSet<(i32,i32)> = HashSet::new();
-    let mut santa_pos = (0,0);
-    let mut robo_pos = (0,0);
+fn parse_instructions_p2(contents: &str) -> HashSet<(i32, i32)> {
+    let mut houses: HashSet<(i32, i32)> = HashSet::new();
+    let mut santa_pos = (0, 0);
+    let mut robo_pos = (0, 0);
 
     houses.insert(santa_pos);
     houses.insert(robo_pos);
 
-    for (index,c) in contents.char_indices() {
+    for (index, c) in contents.char_indices() {
         if index % 2 == 0 {
-            santa_pos = get_new_pos(santa_pos,c);
+            santa_pos = get_new_pos(santa_pos, c);
             houses.insert(santa_pos);
         } else {
-            robo_pos = get_new_pos(robo_pos,c);
+            robo_pos = get_new_pos(robo_pos, c);
             houses.insert(robo_pos);
         }
     }
     houses
 }
 
-fn get_new_pos(current_pos: (i32,i32),c:char) -> (i32,i32) {
-     match c {
-        '^' => (current_pos.0,current_pos.1+1),
-        'v' => (current_pos.0, current_pos.1-1),
-        '>' => (current_pos.0+1, current_pos.1),
-        _ => (current_pos.0-1,current_pos.1),
+fn get_new_pos(current_pos: (i32, i32), c: char) -> (i32, i32) {
+    match c {
+        '^' => (current_pos.0, current_pos.1 + 1),
+        'v' => (current_pos.0, current_pos.1 - 1),
+        '>' => (current_pos.0 + 1, current_pos.1),
+        _ => (current_pos.0 - 1, current_pos.1),
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -65,9 +63,9 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(2,parse_instructions_p1(">").len());
-        assert_eq!(4,parse_instructions_p1("^>v<").len());
-        assert_eq!(2,parse_instructions_p1("^v^v^v^v^v").len());
+        assert_eq!(2, parse_instructions_p1(">").len());
+        assert_eq!(4, parse_instructions_p1("^>v<").len());
+        assert_eq!(2, parse_instructions_p1("^v^v^v^v^v").len());
     }
 
     #[test]
@@ -76,4 +74,3 @@ mod tests {
         assert_eq!(11, parse_instructions_p2("^v^v^v^v^v").len());
     }
 }
-
--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -2,12 +2,10 @@ use std::fs;
 use std::process;
 
 fn main() {
-    let contents = fs::read_to_string("input")
-        .expect("Failed to read input file");
+    let contents = fs::read_to_string("input").expect("Failed to read input file");
 
     if let Err(e) = day_03::run(contents) {
         eprintln!("Could not execute: {e}");
         process::exit(1);
     };
-
 }
