01ea52cfd656eebd05749e5860aad8166632e4a1
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-20 21:18:15 +0530

    Refactored Part 01 to use a HashSet instead of a HashMap

--- day_03/src/lib.rs
+++ day_03/src/lib.rs
@@ -1,11 +1,10 @@
 use std::error::Error;
-use std::collections::HashMap;
 use std::collections::HashSet;
 
 pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
 
     let houses = parse_instructions_p1(&contents);
-    let houses_visited = houses.keys().count();
+    let houses_visited = houses.len();
 
     println!("Santa visited {} houses",houses_visited);
 
@@ -16,22 +15,16 @@ pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_instructions_p1(contents: &str) -> HashMap<(i32,i32),u32> {
-    let mut houses: HashMap<(i32,i32),u32> = HashMap::new();
+fn parse_instructions_p1(contents: &str) -> HashSet<(i32,i32)> {
+    let mut houses: HashSet<(i32,i32)> = HashSet::new();
     // Tracking houses on grid using (x,y) coordinates
     // x is East--West, y is N--S
     let mut current_pos = (0,0);
-    houses.insert(current_pos,1);
+    houses.insert(current_pos);
 
     for c in contents.chars() {
-        let new_pos = match c {
-            '^' => (current_pos.0,current_pos.1+1),
-            'v' => (current_pos.0, current_pos.1-1),
-            '>' => (current_pos.0+1, current_pos.1),
-            _ => (current_pos.0-1,current_pos.1),
-        };
-        current_pos = new_pos;
-        houses.entry(current_pos).and_modify(|p| *p+=1).or_insert(1);
+        current_pos = get_new_pos(current_pos,c);
+        houses.insert(current_pos);
     }
     houses
 }
@@ -43,8 +36,6 @@ fn parse_instructions_p2(contents: &str) -> HashSet<(i32,i32)> {
 
     houses.insert(santa_pos);
     houses.insert(robo_pos);
-
-    let mut current_pos = (0,0);
 
     for (index,c) in contents.char_indices() {
         if index % 2 == 0 {
@@ -75,9 +66,9 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(2,parse_instructions_p1(">").keys().count());
-        assert_eq!(4,parse_instructions_p1("^>v<").keys().count());
-        assert_eq!(2,parse_instructions_p1("^v^v^v^v^v").keys().count());
+        assert_eq!(2,parse_instructions_p1(">").len());
+        assert_eq!(4,parse_instructions_p1("^>v<").len());
+        assert_eq!(2,parse_instructions_p1("^v^v^v^v^v").len());
     }
 
     #[test]
