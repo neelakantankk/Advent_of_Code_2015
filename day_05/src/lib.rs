9bbb5b98ca48a7be48f13cd5fd2132a24274c84f
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-29 21:04:34 +0530

    Completed Day 05, Part 02; Used external crate for regex

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -1,30 +1,51 @@
+extern crate fancy_regex;
+use fancy_regex::Regex;
+use std::env;
 use std::error::Error;
 
-pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
+pub fn run(contents: String) -> Result<(), Box<dyn Error>> {
     let mut count = 0;
-    for line in contents.lines() {
-        if is_nice(line) {
-            count += 1;
+    let var = env::var("PART").unwrap_or_else(|_| 0.to_string());
+    if var == "1" {
+        for line in contents.lines() {
+            if is_nice_p1(line) {
+                count += 1;
+            }
         }
+    } else if var == "2" {
+        for line in contents.lines() {
+            if is_nice_p2(line) {
+                count += 1;
+            }
+        }
+    } else {
+        panic!("Specify PART=1 or PART=2");
     }
-    println!("There are {} nice lines",count);
+
+    println!("There are {} nice lines", count);
     Ok(())
 }
 
-fn is_nice(line: &str) -> bool {
-    if contains_naughty_string(line) {
-        return false;
-    } else if !contains_repeat_char(line) {
-        return false;
-    } else if !contains_three_vowels(line) {
-        return false;
-    }
-    true
+fn is_nice_p1(line: &str) -> bool {
+    !contains_naughty_string(line) && contains_repeat_char(line) && contains_three_vowels(line)
+}
+
+fn is_nice_p2(line: &str) -> bool {
+    contains_distinct_pairs(line) && contains_separated_repeat(line)
+}
+
+fn contains_distinct_pairs(line: &str) -> bool {
+    let re = Regex::new(r"(\w)(\w).*\1\2").unwrap();
+    re.is_match(line).unwrap()
+}
+
+fn contains_separated_repeat(line: &str) -> bool {
+    let re = Regex::new(r"(\w)\w\1").unwrap();
+    re.is_match(line).unwrap()
 }
 
 fn contains_naughty_string(line: &str) -> bool {
-    line.contains("ab") || line.contains("cd") 
-        || line.contains("pq") || line.contains("xy")
+    line.contains("ab") || line.contains("cd") || line.contains("pq") || line.contains("xy")
 }
 
 fn contains_repeat_char(line: &str) -> bool {
@@ -44,16 +65,16 @@ fn contains_three_vowels(line: &str) -> bool {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     #[test]
     fn test_naughty_string() {
-        assert!(contains_naughty_string("plabqy"),"plabqy is nice");
-        assert!(contains_naughty_string("acdwrt"),"acdwrt is nice");
-        assert!(contains_naughty_string("lmnopq"),"lmnopq is nice");
-        assert!(contains_naughty_string("uvwxyzap"),"uvwxyzap is nice");
-        assert!(!contains_naughty_string("acbdpxqy"),"acbdpxqy is naughty");
+        assert!(contains_naughty_string("plabqy"), "plabqy is nice");
+        assert!(contains_naughty_string("acdwrt"), "acdwrt is nice");
+        assert!(contains_naughty_string("lmnopq"), "lmnopq is nice");
+        assert!(contains_naughty_string("uvwxyzap"), "uvwxyzap is nice");
+        assert!(!contains_naughty_string("acbdpxqy"), "acbdpxqy is naughty");
     }
 
     #[test]
@@ -72,5 +93,36 @@ mod tests{
         assert!(contains_repeat_char("aabbccdd"));
         assert!(!contains_repeat_char("abcdef"));
         assert!(!contains_repeat_char("abcdabcdefe"));
+    }
+
+    #[test]
+    fn test_distinct_pair() {
+        assert!(contains_distinct_pairs("xyxy"));
+        assert!(contains_distinct_pairs("aabcdefgaa"));
+        assert!(!contains_distinct_pairs("aaa"));
+    }
+
+    #[test]
+    fn test_separated_repeat() {
+        assert!(contains_separated_repeat("xyx"));
+        assert!(contains_separated_repeat("abcdefeghi"));
+        assert!(contains_separated_repeat("aaa"));
+        assert!(!contains_separated_repeat("aab"));
+        assert!(!contains_separated_repeat("abba"));
+    }
+
+    #[test]
+    fn test_p1() {
+        assert!(is_nice_p1("ugknbfddgicrmopn"));
+        assert!(is_nice_p1("aaa"));
+        assert!(!is_nice_p1("jchzalrnumimnmhp"));
+    }
+
+    #[test]
+    fn test_p2() {
+        assert!(is_nice_p2("qjhvhtzxzqqjkmpb"));
+        assert!(is_nice_p2("xxyxx"));
+        assert!(!is_nice_p2("uurcxstgmygtbstg"));
+        assert!(!is_nice_p2("ieodomkazucvgmuy"));
     }
 }
