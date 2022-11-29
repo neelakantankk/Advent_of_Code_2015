3a2cfebd6ad2b0cbe57ad142d3a11bbe9e5c1682
Neelakantan K.K <neelakantankk@gmail.com>
2022-11-29 18:49:20 +0530

    Added main logic for part 1; No tests added yet

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -12,5 +12,33 @@ pub fn run(contents: String) -> Result<(),Box<dyn Error>> {
 }
 
 fn is_nice(line: &str) -> bool {
+    if contains_naughty_string(line) {
+        return false;
+    } else if !contains_repeat_char(line) {
+        return false;
+    } else if !contains_three_vowels(line) {
+        return false;
+    }
     true
+}
+
+fn contains_naughty_string(line: &str) -> bool {
+    line.contains("ab") || line.contains("cd") 
+        || line.contains("pq") || line.contains("xy")
+}
+
+fn contains_repeat_char(line: &str) -> bool {
+    let mut prev_char = ' ';
+    for c in line.chars() {
+        if c == prev_char {
+            return true;
+        }
+        prev_char = c;
+    }
+    false
+}
+
+fn contains_three_vowels(line: &str) -> bool {
+    let count = line.matches(|c| "aeiou".contains(c)).count();
+    count > 2
 }
